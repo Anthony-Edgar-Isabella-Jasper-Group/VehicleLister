947b185fb8fe231a04a70ed4d4b8c279d298f140
anthonygonzales <anthony.j.gonzales22@gmail.com>
2022-04-11 11:38:44 -0500

    adjusted config

--- migrator.sql
+++ migrator.sql
@@ -1,5 +1,6 @@
 DROP DATABASE IF EXISTS vehicle_list;
 CREATE DATABASE IF NOT EXISTS vehicle_list;
+DROP USER IF EXISTS 'vehiclelistuser'@'localhost';
 CREATE USER 'vehiclelistuser'@'localhost' IDENTIFIED BY 'codeup';
 GRANT ALL ON vehicle_list.* TO 'vehiclelistuser'@'localhost';
 USE vehicle_list;
